226585e31d34b3982d9daed886c28b502e7f7137
HG-ha <1790233968@qq.com>
2026-01-09 01:38:56 +0800

    Update version number to 0.1.2 in pyproject.toml, uv.lock, and __init__.py

--- jadeui/__init__.py
+++ jadeui/__init__.py
@@ -82,7 +82,7 @@ from .router import Router
 from .server import LocalServer
 from .window import Backdrop, Theme, Window
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "JadeView Team"
 __license__ = "MIT"
 
